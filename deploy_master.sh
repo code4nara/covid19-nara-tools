0b0be7ceecff601cde37de467ba6ca687a092ef0
ishizuka <ishizuka@zukatech.com>
2020-04-18 17:53:50 +0900

    Add auto_merge:false option to deploy_master.sh

--- deploy_master.sh
+++ deploy_master.sh
@@ -1,4 +1,6 @@
 curl -X POST -H "Authorization: token ${GITHUB_TOKEN}" \
              -H "Content-Type: application/json" \
              https://api.github.com/repos/code4nara/covid19/deployments \
-             --data '{"ref": "master", "environment": "production"}'}'
+             --data '{"ref":"master", "environment":"production", "auto_merge":false}'
+
+
--- execupdate.sh
+++ execupdate.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -l
 ####n
 #   Stop Covid19nara
 #   The Script to make JSON
